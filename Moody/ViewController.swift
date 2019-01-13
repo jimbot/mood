deacbf13d22d88589d86d9fc2355e787593ca710
James Chen <jameschen1295@gmail.com>
2019-01-12 16:39:31 -0800

    Initial Commit

---
+++ Moody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moody
+//
+//  Created by James Chen on 1/12/19.
+//  Copyright © 2019 James Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
